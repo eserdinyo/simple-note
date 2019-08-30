eaf7f6c56c1d21d152c38db8b66cc62f8bc5d958
eserdinyo <muhammeteser29@gmail.com>
2019-08-30 20:34:09 +0300

    add components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,132 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <textarea
+        @keyup.enter="save"
+        v-model="message"
+        autofocus
+        placeholder="enter something"
+        name
+        id
+        cols="30"
+        rows="5"
+      ></textarea>
+      <button @click="save">Save</button>
+    </div>
+    <div v-if="notes.length > 0" class="notes-wrapper">
+      <note :note="note" v-for="note in notes" :key="'note-'+note.id" />
+    </div>
+    <div v-else class="empty">
+      <p>What a clean sheet!</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import note from "./components/Note";
+import { EventBus } from "./main";
 
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      message: "",
+      notes: [],
+      notes_key: "NOTES_KET"
+    };
+  },
   components: {
-    HelloWorld
+    note
+  },
+  methods: {
+    save() {
+      if (this.message) {
+        let msg = {
+          id: Math.random(),
+          message: this.message
+        };
+
+        this.notes.push(msg);
+        this.message = "";
+      }
+    }
+  },
+  created() {
+    if (localStorage.getItem(this.notes_key)) {
+      this.notes = JSON.parse(localStorage.getItem(this.notes_key));
+    }
+
+    //remove note
+    EventBus.$on("remove", id => {
+      this.notes = this.notes.filter(note => note.id != id);
+    });
+  },
+  watch: {
+    notes() {
+      localStorage.setItem(this.notes_key, JSON.stringify(this.notes));
+    }
   }
-}
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body {
+  background-color: #202528;
+}
+
+.container {
+  width: 1200px;
+  margin: 0 auto;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.notes-wrapper {
+  width: 1200px;
+  margin: 0 auto;
+  margin-top: 50px;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-gap: 20px;
+}
+
+textarea {
+  background-color: transparent;
+  border: 1px solid #646464;
+  width: calc(100% - 60px);
+  border-radius: 1px;
+  padding: 20px 30px;
+  color: #fff;
+  outline: 0;
+  font-size: 16px;
+  resize: none;
+  margin-top: 50px;
+  font-family: "Chilanka", cursive;
+  display: inline-block;
+}
+
+button {
+  padding: 5px 20px;
+  background-color: transparent;
+  color: #fff;
+  margin-top: 20px;
+  border-radius: 1px;
+  cursor: pointer;
+  font-size: 14px;
+
+  transition: all 0.2s;
+  outline: 0;
+
+  &:hover {
+    border-color: #191919;
+    background-color: #191919;
+  }
+}
+
+.empty {
+  text-align: center;
+  font-size: 80px;
+  color: #646464;
+  font-family: "Chilanka", cursive;
+  text-transform: uppercase;
 }
 </style>
---
+++ src/components/Note.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="note">
+    <p>{{note.message}}</p>
+    <trash-2-icon @click="remove(note.id)" size="1x" class="trash-icon"></trash-2-icon>
+  </div>
+</template>
+
+<script>
+import { Trash2Icon } from "vue-feather-icons";
+import { EventBus } from "../main";
+export default {
+  props: {
+    note: {
+      type: Object,
+      required: true
+    }
+  },
+  components: {
+    Trash2Icon
+  },
+  methods: {
+    remove(id) {
+      EventBus.$emit("remove", id);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.note {
+  color: #fff;
+  font-family: sans-serif;
+  font-weight: 300;
+  border: 1px solid #646464;
+  padding: 5px 10px;
+  border-radius: 1px;
+  font-size: 13px;
+  word-break: break-all;
+  min-height: 100px;
+  line-height: 18px;
+  font-family: "Chilanka", cursive;
+
+  position: relative;
+
+  &:hover .trash-icon {
+    opacity: 1;
+  }
+}
+
+.trash-icon {
+  position: absolute;
+  right: -6px;
+  top: -6px;
+  opacity: 0;
+  cursor: pointer;
+  transition: all 0.2s;
+  user-select: none;
+}
+</style>
